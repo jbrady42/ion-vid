0044e6881a2aafd65d5426e677cf0732d059429c
Jason <jabrady42@gmail.com>
2020-05-15 15:20:21 -0500

    Use client and producer for ion-load-tool

--- types.go
+++ types.go
@@ -1,53 +1,13 @@
 package main
 
-import "github.com/pion/webrtc/v2"
-
-type RoomInfo struct {
-	Rid string `json:"rid"`
-	Uid string `json:"uid"`
-}
+import "github.com/pion/ion/pkg/node/biz"
 
 type ChatInfo struct {
 	Msg        string `json:"msg"`
 	SenderName string `json:"senderName"`
 }
 
-type UserInfo struct {
-	Name string `json:"name"`
-}
-
-type PublishOptions struct {
-	Codec      string `json:"codec"`
-	Resolution string `json:"resolution"`
-	Bandwidth  int    `json:"bandwidth"`
-	Audio      bool   `json:"audio"`
-	Video      bool   `json:"video"`
-	Screen     bool   `json:"screen"`
-}
-
-type JoinMsg struct {
-	RoomInfo
-	Info UserInfo `json:"info"`
-}
-
 type ChatMsg struct {
-	RoomInfo
+	biz.RoomInfo
 	Info ChatInfo `json:"info"`
-}
-
-type PublishMsg struct {
-	RoomInfo
-	Jsep    webrtc.SessionDescription `json:"jsep"`
-	Options PublishOptions            `json:"options"`
-}
-
-func newPublishOptions() PublishOptions {
-	return PublishOptions{
-		Codec:      "h264",
-		Resolution: "hd",
-		Bandwidth:  1024,
-		Audio:      true,
-		Video:      true,
-		Screen:     false,
-	}
 }
